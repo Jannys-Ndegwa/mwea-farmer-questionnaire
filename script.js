71a59820a7341dab4df3f1890824678bbc0b1377
Jannys-Ndegwa <jannysndegwa@gmail.com>
2026-05-18 01:24:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,13 @@ form.addEventListener('submit', async (e) => {
 
     const formData = new FormData(form);
 
-    const data = {};
-
-    formData.forEach((value, key) => {
-        data[key] = value;
-    });
-
-    try{
+    try {
 
         const response = await fetch(
-            "https://script.google.com/macros/s/AKfycbwNYQNdQnyvDwTqYcUSOE9KzacsHEJU5_piHOHiOPCSzwpLOxm0V-V8Lsr4eK3WmH6ySw/exec",
+            "https://script.google.com/macros/s/AKfycbwCPn-hHhckh6PDsNP53IF6wPD3HHhHtL8FWJ_bCO0WEzcaJ4t3i4q7U53polxPghu85g/exec",
             {
-                method:"POST",
-                body:JSON.stringify(data)
+                method: "POST",
+                body: formData
             }
         );
 
@@ -27,7 +21,9 @@ form.addEventListener('submit', async (e) => {
 
         form.reset();
 
-    }catch(error){
+    } catch(error) {
+
+        console.error(error);
 
         document.getElementById("responseMessage").innerText =
             "Failed to submit.";
@@ -35,7 +31,3 @@ form.addEventListener('submit', async (e) => {
     }
 
 });
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
-}
